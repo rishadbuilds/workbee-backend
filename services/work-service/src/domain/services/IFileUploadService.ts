00c1bd52818a6c6f1eb7716550d6cd04ccd5f109
rishadsinu <rishadsinu2233@gmail.com>
2026-09-13 15:56:37 +0530

    chores: removed multer settup in work service

--- services/work-service/src/domain/services/IFileUploadService.ts
+++
@@ -1,4 +0,0 @@
-export interface IFileUploadService {
-  saveFile(file: Express.Multer.File, folder: string): Promise<string>;
-  deleteFile(filePath: string): Promise<void>;
-}
--- services/work-service/src/infrastructure/di/services.ts
+++ services/work-service/src/infrastructure/di/services.ts
@@ -4,16 +4,13 @@ import { container } from "tsyringe";
 import { HashService } from "../services/HashServices";
 
 import { IHashService } from "../../domain/services/IHashService";
-import { IFileUploadService } from "../../domain/services/IFileUploadService";
 
-import { FileUploadService } from "../services/FileUploadService";
 import { IEmailService } from "../../domain/services/IEmailService";
 import { EmailService } from "../services/EmailService";
 import { CloudinaryService } from "../services/CloudinaryService";
 import { ICloudinaryService } from "../../domain/services/ICloudinaryService";
 
 container.registerSingleton<IHashService>("HashService",HashService)
-container.registerSingleton<IFileUploadService>("FileUploadService",FileUploadService)
 container.registerSingleton<IEmailService>("EmailService",EmailService)
 
 //cloudinary
